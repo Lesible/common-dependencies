e4964e1aa02d82dd2ce5ede520c9397c76c5c163
Lesible <>
2021-11-04 15:40:12 +0800

    update to 2.0.1 add CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [2.0.1] 2021-11-04
+- add common return object  `Result`
+
+## [2.0.0] 2021-10-28
+- init program for TagVV 
---
+++ commons-utils/src/main/java/com/sumwhy/model/Result.java
@@ -0,0 +1,75 @@
+package com.sumwhy.model;
+
+/**
+ * <p> 返回结果体 </p>
+ * <p> created at 2021-11-04 15:30 by lesible </p>
+ *
+ * @author 何嘉豪
+ */
+public class Result<T> {
+
+    public static final Result<Void> SUCCESS_RESULT = new Result<>(0, "success", null);
+
+    public static final Result<Void> FAIL_RESULT = new Result<>(500, "fail", null);
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public Result(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Result() {
+    }
+
+    public static <T> Result<T> successData(T data) {
+        return new Result<>(0, "success", data);
+    }
+
+    public static Result<Void> errorMsg(String msg) {
+        return new Result<>(500, msg, null);
+    }
+
+    public static Result<Void> errorMsg(Integer code, String msg) {
+        return new Result<>(code, msg, null);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
+
