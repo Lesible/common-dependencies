daf9efc027a2789bcd67875b7e7ef62a1956bcb6
Lesible <>
2021-11-19 17:03:01 +0800

    修改版本

--- commons-utils/src/main/java/com/sumwhy/util/JsonUtil.java
+++ commons-utils/src/main/java/com/sumwhy/util/JsonUtil.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.type.ReferenceType;
 import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,11 +27,13 @@ public class JsonUtil {
 
     static {
         OBJECT_MAPPER.findAndRegisterModules();
+        OBJECT_MAPPER.setTimeZone(TimeZone.getDefault());
         OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         OBJECT_MAPPER.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
         FAST_JSON_LIKE_OBJECT_MAPPER.findAndRegisterModules();
+        FAST_JSON_LIKE_OBJECT_MAPPER.setTimeZone(TimeZone.getDefault());
         FAST_JSON_LIKE_OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         FAST_JSON_LIKE_OBJECT_MAPPER.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         FAST_JSON_LIKE_OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
@@ -77,6 +78,8 @@ public class JsonUtil {
      * @param source           来源
      * @param parametrized     范型基本类
      * @param parameterClasses 范型上面的类
+     * @param <T>              Class 的范型
+     * @return 拷贝后的结果
      */
     public static <T> T deepClone(Object source, Class<?> parametrized, Class<?>... parameterClasses) {
         JavaType javaType = constructType(parametrized, parameterClasses);
@@ -88,6 +91,8 @@ public class JsonUtil {
      *
      * @param source   来源
      * @param javaType 类型
+     * @param <T>      Class 的范型
+     * @return 拷贝后的结果
      */
     public static <T> T deepClone(Object source, JavaType javaType) {
         return OBJECT_MAPPER.convertValue(source, javaType);
@@ -98,6 +103,8 @@ public class JsonUtil {
      *
      * @param source 来源
      * @param target 目标
+     * @param <T>    Class 的范型
+     * @return 更新后的数据
      */
     public static <T> T updateValue(Object source, T target) {
         try {
